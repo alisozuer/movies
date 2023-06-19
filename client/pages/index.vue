57c10c5e918aa30eae618d1c6a2df997202673a6
alisozuer <78449385+alisozuer@users.noreply.github.com>
2023-06-20 02:38:06 +0300

    css edit

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -36,7 +36,7 @@
 .card {
   position: relative;
   width: 100%;
-  height: 350px;
+  height: 300px;
   display: block;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
 
